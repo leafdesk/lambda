98237b67cfe4ed6496aeb147925efda4fc59a3db
leafdesk <lsg001008@gmail.com>
2024-06-21 13:47:33 +0900

    test: event.body에서 sessionId 추출

--- teleagent/src/handler.ts
+++ teleagent/src/handler.ts
@@ -1,7 +1,21 @@
-export const handler = async (event: any) => {
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  if (!event.body) {
+    throw new Error('Request body is missing or empty')
+  }
+  const { sessionId } = JSON.parse(event.body)
+  console.log('sessionId:', sessionId)
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-};
+    body: JSON.stringify('Hello from Lambda!'), // ChatGPT 최종 응답.
+  }
+  return response
+}
