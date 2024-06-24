de1f08b3492e9dc11a5724ae4ecaa939aa6c8ae2
leafdesk <lsg001008@gmail.com>
2024-06-24 11:47:51 +0900

    feat: Chat API handler에 GET, POST method 분기

--- teleagent/src/handler.ts
+++ teleagent/src/handler.ts
@@ -3,19 +3,96 @@ import {
   APIGatewayProxyHandler,
   APIGatewayProxyResult,
 } from 'aws-lambda'
+import {
+  getChatSession,
+  saveChatSession,
+  updateChatSession,
+} from './chat-service'
 
+/**
+ * GET /chat 요청.
+ */
+const handleGetChat = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  const sessionId = event.queryStringParameters?.sessionId
+  if (!sessionId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ ok: false, message: 'sessionId is required' }),
+    }
+  }
+  const session = await getChatSession(sessionId)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true, session }),
+  }
+}
+
+/**
+ * POST /chat 요청.
+ */
+const handlePostChat = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  const {
+    sessionId,
+    userPhoneNumber,
+    messages,
+    startTime,
+    endTime,
+    sessionStatus,
+  } = JSON.parse(event.body!)
+  const existingSession = await getChatSession(sessionId)
+
+  if (existingSession) {
+    await updateChatSession(
+      sessionId,
+      userPhoneNumber,
+      messages,
+      startTime,
+      endTime,
+      sessionStatus,
+    )
+  } else {
+    await saveChatSession(
+      sessionId,
+      userPhoneNumber,
+      messages,
+      startTime,
+      endTime,
+      sessionStatus,
+    )
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true }),
+  }
+}
+
+/**
+ * API 핸들러.
+ */
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  if (!event.body) {
-    throw new Error('Request body is missing or empty')
+  if (event.httpMethod === 'GET' && event.path === '/chat') {
+    return handleGetChat(event)
+  } else if (event.httpMethod === 'POST' && event.path === '/chat') {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Request body is missing or empty' }),
+      }
+    }
+    return handlePostChat(event)
   }
-  const { sessionId } = JSON.parse(event.body)
-  console.log('sessionId:', sessionId)
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'), // ChatGPT 최종 응답.
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ message: 'Not Found' }),
   }
-  return response
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify('Hello from Lambda!'), // ChatGPT 최종 응답.
+  // }
 }
