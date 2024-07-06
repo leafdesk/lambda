77962694f4747427405b69f60b76ddd56c4ceb64
leafdesk <lsg001008@gmail.com>
2024-07-06 21:39:52 +0900

    feat: Amazon Connect 연동을 위해 action 파라미터 추가
    handlePostInbound 메서드에서 dynamoDB에 접근하고, flag.Payload 조회.

--- teleagent/src/handlers/router.ts
+++ teleagent/src/handlers/router.ts
@@ -3,6 +3,13 @@ import { handleGetChat, handlePostChat } from './chat-handler'
 import { handlePostInbound } from './inbound-handler'
 
 export const route: APIGatewayProxyHandler = async (event, context) => {
+  const body = event.body && JSON.parse(event.body)
+  const action = body?.action
+
+  if (action && action === 'inbound') {
+    // Amazon Connect 연동을 위해 action 파라미터 사용.
+    return handlePostInbound(event)
+  }
   if (event.path === '/chat' && event.httpMethod === 'GET') {
     return handleGetChat(event)
   } else if (event.path === '/chat' && event.httpMethod === 'POST') {
